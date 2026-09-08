e928f6b819896a0f3f7262db8b1631b871bda607
Carlos Jesús Martínez Martínez <carlosjesusmartinezmartinez@MacBook-Air-de-Carlos.local>
2026-09-07 22:38:39 -0600

    feat: configurar cliente de Fake Store API

---
+++ app/src/main/java/com/example/shopicaros/data/remote/FakeStoreApi.kt
@@ -0,0 +1,19 @@
+package com.example.shopicaros.data.remote
+
+import com.example.shopicaros.data.model.Product
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FakeStoreApi {
+
+    @GET("products")
+    suspend fun getProducts(): List<Product>
+
+    @GET("products/categories")
+    suspend fun getCategories(): List<String>
+
+    @GET("products/category/{category}")
+    suspend fun getProductsByCategory(
+        @Path("category") category: String
+    ): List<Product>
+}
