818a0e45045e710e59849572033a23484243f971
Carlos Jesús Martínez Martínez <carlosjesusmartinezmartinez@MacBook-Air-de-Carlos.local>
2026-09-07 22:23:55 -0600

    chore: configurar acceso a API y dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 }
